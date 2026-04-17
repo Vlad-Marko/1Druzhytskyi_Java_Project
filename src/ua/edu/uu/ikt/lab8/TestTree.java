bbd964eb8c0e93dedbb96ab928018b462e6f1173
Владислав Дружицький <druzhytskyivlad@gmail.com>
2026-04-17 16:11:05 +0300

    9.0

--- src/ua/edu/uu/ikt/lab8/TestTree.java
+++ src/ua/edu/uu/ikt/lab8/TestTree.java
@@ -18,5 +18,66 @@ public class TestTree {
         System.out.println("Max node is: " + tree.findMaxNode());
         System.out.println("Min node is: " + tree.findMinNode().getData());
         System.out.println("Max node is: " + tree.findMaxNode().getData());
-    }
+
+         System.out.println("9 практична");
+         tree = new Tree();
+
+         tree.insert(35);
+         tree.insert(24);
+         tree.insert(15);
+         tree.insert(50);
+         tree.insert(45);
+         tree.insert(44);
+         tree.insert(51);
+
+         tree.displayTree();
+         System.out.println("Depth of the tree is: " + tree.getDepth());
+         System.out.println("Min node is: " + tree.findMinNode());
+         System.out.println("Max node is: " + tree.findMaxNode());
+
+         System.out.println("Min node is: " + tree.findMinNode().getData());
+         System.out.println("Max node is: " + tree.findMaxNode().getData());
+
+         tree.delete(44);
+         tree.displayTree();
+         System.out.println("Depth of the tree is: " + tree.getDepth());
+         System.out.println("Min node is: " + tree.findMinNode());
+         System.out.println("Max node is: " + tree.findMaxNode());
+
+         System.out.println("Min node is: " + tree.findMinNode().getData());
+         System.out.println("Max node is: " + tree.findMaxNode().getData());
+
+
+         System.out.println("5 завдання");
+         tree = new Tree();
+
+         tree.insert(35);
+         tree.insert(24);
+         tree.insert(15);
+         tree.insert(50);
+         tree.insert(45);
+         tree.insert(44);
+         tree.insert(51);
+
+         tree.displayTree();
+         System.out.println("Depth of the tree is: " + tree.getDepth());
+         System.out.println("Min node is: " + tree.findMinNode());
+         System.out.println("Max node is: " + tree.findMaxNode());
+
+         System.out.println("Min node is: " + tree.findMinNode().getData());
+         System.out.println("Max node is: " + tree.findMaxNode().getData());
+
+         tree.delete(50);
+         tree.displayTree();
+         System.out.println("Depth of the tree is: " + tree.getDepth());
+         System.out.println("Min node is: " + tree.findMinNode());
+         System.out.println("Max node is: " + tree.findMaxNode());
+
+         System.out.println("Min node is: " + tree.findMinNode().getData());
+         System.out.println("Max node is: " + tree.findMaxNode().getData());
+
+
+}
+
+
 }
--- src/ua/edu/uu/ikt/lab8/Tree.java
+++ src/ua/edu/uu/ikt/lab8/Tree.java
@@ -117,4 +117,89 @@ public class Tree {
         }
     }
 
+
+    public boolean delete(int key) {
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChild = true;
+        //1. Шукаємо у дереві вузол для видалення
+        while (current.getData() != key) {
+            parent = current;
+            if (key < current.getData()) {
+                isLeftChild = true;
+                current = current.getLeft();
+            } else {
+                isLeftChild = false;
+                current = current.getRight();
+            }
+            if (current == null)
+
+// 2. Якщо вузол відсутній – нічого не робимо і повертаємо false
+                return false;
+        }
+//3.1 Якщо вузол лист – видаляємо посилання на вузол в батьківського вузла
+        if (current.getLeft() == null && current.getRight() == null) {
+            if (current == root)
+                root = null;
+            else if (isLeftChild)
+                parent.setLeft(null);
+            else
+                parent.setRight(null);
+        }
+//3.2  Якщо вузол має лівого нащадка – ставимо лівого нащадка на місце вузла. Тобто, у батьківського вузла міняємо посилання так, що замість вузла що видаляється стає його лівий нащадок
+        else if (current.getRight() == null) {
+            if (current == root)
+                root = current.getLeft();
+            else if (isLeftChild)
+                parent.setLeft(current.getLeft());
+            else
+                parent.setRight(current.getLeft());
+        } else if (current.getLeft() == null) {
+            if (current == root)
+                root = current.getRight();
+            else if (isLeftChild)
+                parent.setLeft(current.getRight());
+            else
+                parent.setRight(current.getRight());
+        }
+//3.3  Якщо вузол має двох нащадків:
+//        Знаходимо наступника вузла.
+//                У наступника ставимо лівим нащадком лівий вузол  вузла, що видаляється.
+//                У батьківського вузла ставимо правим нащадком наступника.
+        else {
+            Node successor = prepareSuccessor(current);
+
+            if (current == root)
+                root = successor;
+            else if (isLeftChild)
+                parent.setLeft(successor);
+            else
+                parent.setRight(successor);
+
+            return true;
+        }
+        return false;
+    }
+
+    private Node prepareSuccessor(Node delNode) {
+        Node successorParent = delNode;
+        Node successor = delNode;
+        Node current = delNode.getRight();
+        while (current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.getLeft();
+        }
+
+        if (successor != delNode.getRight()) {
+            successorParent.setLeft(successor.getRight());
+            successor.setRight(delNode.getRight());
+        }
+
+        successor.setLeft(delNode.getLeft());
+
+        return successor;
+    }
+
+
 }
